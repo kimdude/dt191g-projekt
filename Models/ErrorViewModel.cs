6df45e5253aecdec5fc9368516ca4dbe12ef4460
Kim Dudenhöfer <kidu2400@student.miun.se>
2026-04-09 16:15:55 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace bookingApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
